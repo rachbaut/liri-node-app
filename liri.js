2d78569efb07e9753bc0738221dbe6c8fae3b4c7
Rachel Bautista <rachelbaut5@gmail.com>
2019-01-20 23:33:49 -0800

    new code changes

--- liri.js
+++ liri.js
@@ -1,1 +1,31 @@
 require("dotenv").config();
+
+var fs = require("fs");
+var keys = require("./keys"); //i don't this right, if I am not using the spotify keys?
+var axios = require("axios");
+
+var search = process.argv[2];
+var term = process.argv.slice(3).join(" "); //would I need this if I am following a different example?
+
+
+
+var Concert = function () {
+    this.findArtist = function (artist) {
+        var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(URL)
+            .then(function (response) {
+                var conResData = response.data[0];
+                var concertData = [
+                    "Name: " + conResData.name,
+                    "Location: " + conResData.location,
+                    "Date: " + conResData.date
+                ]
+                console.log(concertData);
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
+    }
+};
+
+module.exports = Concert;
